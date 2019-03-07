b588d4c79d8ae23bb8d266d6747e6a7dd32cb26b
hxhb <hxhb@live.com>
2019-03-08 01:12:55 +0800

    add copy Register.bat

--- PullBinaries.bat
+++ PullBinaries.bat
@@ -4,6 +4,7 @@ md UE4Launcher\Engine\Content\Internationalization\icudt53l
 md UE4Launcher\Engine\Content\Slate 
 md UE4Launcher\Engine\Shaders\StandaloneRenderer
 copy ..\..\..\Binaries\Win64\UE4Launcher.exe UE4Launcher\Engine\Binaries\Win64
+copy Register.reg UE4Launcher\
 xcopy /y/i/s/e ..\..\..\Content\Internationalization\English\icudt53l UE4Launcher\Engine\Content\Internationalization\icudt53l
 xcopy /y/i/s/e ..\..\..\Content\Slate UE4Launcher\Engine\Content\Slate
 xcopy /y/i/s/e ..\..\..\Shaders\StandaloneRenderer UE4Launcher\Engine\Shaders\StandaloneRenderer
