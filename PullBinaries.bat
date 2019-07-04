1e8f3a0869da4f98a0c217113781d7f5357fcfe5
e <hxhb@live.com>
2019-07-04 13:48:30 +0800

    additional pull icon

--- PullBinaries.bat
+++ PullBinaries.bat
@@ -8,5 +8,6 @@ copy Register.reg UE4Launcher\
 xcopy /y/i/s/e ..\..\..\Content\Internationalization\English\icudt53l UE4Launcher\Engine\Content\Internationalization\icudt53l
 xcopy /y/i/s/e ..\..\..\Content\Slate UE4Launcher\Engine\Content\Slate
 xcopy /y/i/s/e ..\..\..\Shaders\StandaloneRenderer UE4Launcher\Engine\Shaders\StandaloneRenderer
-echo "start .\Engine\Binaries\Win64\UE4Launcher.exe">UE4Launcher\UE4Launcher.bat
+copy Resources\icon.ico UE4Launcher\Engine\Content\Slate\Icons
+echo "Engine\Binaries\Win64\UE4Launcher.exe">UE4Launcher\UE4Launcher.bat
 
