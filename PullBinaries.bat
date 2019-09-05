66b8915fc18c50fba9284818c26bfe68e7ffefe4
hxhb <hxhb@live.com>
2019-09-05 13:38:38 +0800

    commit pull binaries

--- PullBinaries.bat
+++ PullBinaries.bat
@@ -1,13 +1,14 @@
 @echo off
-md UE4Launcher\Engine\Binaries\Win64
-md UE4Launcher\Engine\Content\Internationalization\icudt53l
-md UE4Launcher\Engine\Content\Slate 
-md UE4Launcher\Engine\Shaders\StandaloneRenderer
-copy ..\..\..\Binaries\Win64\UE4Launcher.exe UE4Launcher\Engine\Binaries\Win64
-copy Register.reg UE4Launcher\
-xcopy /y/i/s/e ..\..\..\Content\Internationalization\English\icudt53l UE4Launcher\Engine\Content\Internationalization\icudt53l
-xcopy /y/i/s/e ..\..\..\Content\Slate UE4Launcher\Engine\Content\Slate
-xcopy /y/i/s/e ..\..\..\Shaders\StandaloneRenderer UE4Launcher\Engine\Shaders\StandaloneRenderer
-copy Resources\icon.ico UE4Launcher\Engine\Content\Slate\Icons
-echo "Engine\Binaries\Win64\UE4Launcher.exe">UE4Launcher\UE4Launcher.bat
+set PROJECT_NAME=UE4Launcher
+md %PROJECT_NAME%\Engine\Binaries\Win64
+md %PROJECT_NAME%\Engine\Content\Internationalization\icudt53l
+md %PROJECT_NAME%\Engine\Content\Slate 
+md %PROJECT_NAME%\Engine\Shaders\StandaloneRenderer
+copy ..\..\..\Binaries\Win64\%PROJECT_NAME%.exe %PROJECT_NAME%\Engine\Binaries\Win64
+copy Register.reg %PROJECT_NAME%\
+xcopy /y/i/s/e ..\..\..\Content\Internationalization\English\icudt53l %PROJECT_NAME%\Engine\Content\Internationalization\icudt53l
+xcopy /y/i/s/e ..\..\..\Content\Slate %PROJECT_NAME%\Engine\Content\Slate
+xcopy /y/i/s/e ..\..\..\Shaders\StandaloneRenderer %PROJECT_NAME%\Engine\Shaders\StandaloneRenderer
+copy Resources\icon.ico %PROJECT_NAME%\Engine\Content\Slate\Icons
+echo "Engine\Binaries\Win64\%PROJECT_NAME%.exe">%PROJECT_NAME%\%PROJECT_NAME%.bat
 
