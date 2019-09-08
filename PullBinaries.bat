ed264d0d131d08778423d7288b5f094396eef8a0
hxhb <hxhb@live.com>
2019-09-08 23:09:46 +0800

    add unregister

--- PullBinaries.bat
+++ PullBinaries.bat
@@ -6,6 +6,7 @@ md %PROJECT_NAME%\Engine\Content\Slate
 md %PROJECT_NAME%\Engine\Shaders\StandaloneRenderer
 copy ..\..\..\Binaries\Win64\%PROJECT_NAME%.exe %PROJECT_NAME%\Engine\Binaries\Win64
 copy Register.reg %PROJECT_NAME%\
+copy UnRegister.reg %PROJECT_NAME%\
 xcopy /y/i/s/e ..\..\..\Content\Internationalization\English\icudt53l %PROJECT_NAME%\Engine\Content\Internationalization\icudt53l
 xcopy /y/i/s/e ..\..\..\Content\Slate %PROJECT_NAME%\Engine\Content\Slate
 xcopy /y/i/s/e ..\..\..\Shaders\StandaloneRenderer %PROJECT_NAME%\Engine\Shaders\StandaloneRenderer
